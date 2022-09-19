988438adfd2b57539685be96202b58151bad0c96
LuizFDOS <luizfernando.os@outlook.com>
2022-09-18 22:23:26 -0300

    Create initial styling 💅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-function App() {
+// JSX = JavaScript + XML
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
+import "./global.css";
+
+export function App() {
   return (
-  <h1>Hello world</h1>
-    )
-}
+    <div>
+      <Header />
+      <Post
+        author="Luiz Fernando"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non rem voluptate culpa tempore cumque rerum at. Rerum velit architecto labore aspernatur deleniti quam eos odit voluptatibus magni. Suscipit beatae cumque natus ratione, modi facilis, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
+      />
 
-export default App
+      <Post
+        author="João Martinz"
+        content="Lorem ipsum dolor sit, amet consectetur, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
+      />
+    </div>
+  );
+}
