1fda402f95ea853f6ff9a522dc919ef6a576746a
LuizFDOS <luizfernando.os@outlook.com>
2022-09-19 19:40:37 -0300

    Create and add Sidebar component 💄

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 // JSX = JavaScript + XML
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
+import style from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Luiz Fernando"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non rem voluptate culpa tempore cumque rerum at. Rerum velit architecto labore aspernatur deleniti quam eos odit voluptatibus magni. Suscipit beatae cumque natus ratione, modi facilis, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
-      />
+      <div className={style.wrapper}>
+        <Sidebar>Sidebar</Sidebar>
 
-      <Post
-        author="João Martinz"
-        content="Lorem ipsum dolor sit, amet consectetur, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
-      />
+        <main>
+          <Post
+            author="Luiz Fernando"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non rem voluptate culpa tempore cumque rerum at. Rerum velit architecto labore aspernatur deleniti quam eos odit voluptatibus magni. Suscipit beatae cumque natus ratione, modi facilis, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
+          />
+
+          <Post
+            author="João Martinz"
+            content="Lorem ipsum dolor sit, amet consectetur, aliquam qui, odio impedit amet laborum dolor nemo temporibus perferendis? Reiciendis quia totam quidem tempora dolorem."
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { PencilLine } from "phosphor-react";
+
+import style from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={style.sidebar}>
+      <img
+        className={style.cover}
+        src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={style.profile}>
+        <img
+          src="https://github.com/luizfdos.png"
+          alt=""
+          className={style.avatar}
+        />
+        <strong>Luiz Fernando</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
